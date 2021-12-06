0e32eff1a2d4a3a182006ecbd964c49752b39735
Trevor L <trevorlong992@gmail.com>
2021-12-05 19:37:49 -0500

    main function should be done with this. Wont take too much longer

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,27 @@
 // Variable declaration 
 var currentTime = moment();
+var hourBlocks;
+var thisHour;
+var eventText = $("eventtext");
+var eventBlock = $(".col-8");
+$.each(eventText, function () {
+    this.value = "";
+});
 
-console.log(currentTime);
+// Main function to determine if the time is then, now, or later. Offset by number of IDs
+
+function schedulerTimeNow() {
+    eventBlock.removeClass("then now later");
+    $.each(eventBlock, function (timeBlockSet) {
+        if (timeBlockSet < (currentTime.hour() - 9)) {
+            $(this).addClass("then");
+            }
+        else if (timeBlockSet == (currentTime.hour() - 9)) {
+            $(this).addClass("now");
+            }
+        else {
+            $(this).addClass("later");
+            }
+    });
+};
+thisHour = currentTime.hour();
