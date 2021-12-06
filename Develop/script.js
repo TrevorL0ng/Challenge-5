9c334dcda6e2ab8eb3f2486f97413be9dc9dcc68
Trevor L <trevorlong992@gmail.com>
2021-12-05 20:06:54 -0500

    Adding more small bits, roadblock coming after dinner. Spaghetti was good

--- Develop/script.js
+++ Develop/script.js
@@ -8,20 +8,26 @@ $.each(eventText, function () {
     this.value = "";
 });
 
+// Displaying the current day 
+
+$("#currentDay").text(`${currentTime.format('dddd, MMMM Do')}`);
+
 // Main function to determine if the time is then, now, or later. Offset by number of IDs
 
 function schedulerTimeNow() {
     eventBlock.removeClass("then now later");
     $.each(eventBlock, function (timeBlockSet) {
         if (timeBlockSet < (currentTime.hour() - 9)) {
-            $(this).addClass("then");
+            $(this).addClass("past");
             }
         else if (timeBlockSet == (currentTime.hour() - 9)) {
-            $(this).addClass("now");
+            $(this).addClass("present");
             }
         else {
-            $(this).addClass("later");
+            $(this).addClass("future");
             }
     });
 };
 thisHour = currentTime.hour();
+
+
